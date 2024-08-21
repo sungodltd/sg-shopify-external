0f3c5f8a5e5e3636938956aa0675b5c0ca7c46fb
Rob Watkiss <rob@sungod.co>
2024-08-21 15:46:56 +0100

    Remove all image assigning logic to create-on-the-fly-bundles cart transform

--- extensions/create-on-the-fly-bundles/src/index.ts
+++ extensions/create-on-the-fly-bundles/src/index.ts
@@ -9,70 +9,37 @@ const NO_CHANGES = {
   operations: [],
 } as FunctionRunResult
 
-function cleanSKU (sku: string): string {
-  // Remove lens techs
-  const pattern = /le_(N|P|8P|8|[A-Z]{0,3}RX)?([a-z]*)(N|8N)?/
-  sku = sku.replace(pattern, 'le_$2')
-
-  // Map all matte parts to infinite (_M to _I)
-  sku = sku.replace(/_M/, '_I')
-
-  return sku
-}
-
-type ProductImage = {
-  url: string
-}
-function getProductImage(lines: typeof RunInput["cart"]["lines"]): ProductImage | null {
-  try {
-    // Get the SKUs of all parts in this bundle
-    let bundlePartSKUs = lines.map(
-      (line) => cleanSKU(line.merchandise.sku)
-    ).filter(Boolean) as string[]
-
-    const image = lines.map((line) => {
-      const images = line.merchandise.productImages?.jsonValue
-      if (!images) return null
-      const partsKey = bundlePartSKUs.filter(
-        sku => images?.validSKUPrefixes.includes(sku.split('_')[0])
-      ).sort((a, b) => a.localeCompare(b)).join(',')
-      console.error(partsKey)
-      if (!partsKey) return null
-      if (partsKey in images.mapping) {
-        return {
-          url: images.baseURL + images.mapping[partsKey]
-        }
-      }
-    }).find(Boolean) as ProductImage
-
-    console.error(image?.url)
-
-    return image
-
-  } catch (error) {
-    console.error("Error getting product image")
-    console.error(error)
-    return null
-  }
-}
-
 export function run(input: RunInput): FunctionRunResult {
-  console.error("Running cart...");
+  // console.error("Running cart...");
 
   // Get all line items and group them by their bundleId. Ignore if they don't have a bundleId.
   type GroupedBundles = {
-    [bundleId: string]: typeof input.cart.lines
-  }
-  const bundles: GroupedBundles = {}
-  input.cart.lines.forEach((line) => {
-    if (!line.bundleId?.value) return;
-    const bundleId = line.bundleId.value as string;
-    if (!bundles[bundleId]) {
-      bundles[bundleId] = [];
+    [bundleId: string]: {
+      parentVariantId: string,
+      items: typeof input.cart.lines,
+      attributes: Record<string, string>
     }
-    bundles[bundleId].push(line);
-  })
-  console.error(bundles)
+  }
+  const bundles = input.cart.lines.reduce((acc, line) => {
+    if (!line.bundleId?.value) return acc;
+    const bundleId = line.bundleId.value;
+    if (!acc[bundleId]) acc[bundleId] = {
+      items: [],
+      parentVariantId: '',
+      attributes: { _bundleId: bundleId }
+    };
+    acc[bundleId].items.push(line);
+
+    acc[bundleId].parentVariantId = line.parentVariantId?.value || acc[bundleId].parentVariantId
+
+    acc[bundleId].attributes['_checkoutInfo'] = line.checkoutInfo?.value || acc[bundleId].attributes['_checkoutInfo']
+    acc[bundleId].attributes['_despatchDate'] = line.despatchDate?.value || acc[bundleId].attributes['_despatchDate']
+    acc[bundleId].attributes['_bundleSku'] = line.bundleSku?.value || acc[bundleId].attributes['_bundleSku']
+
+    return acc;
+  }, {} as GroupedBundles)
+
+  // console.error(bundles)
 
   // If there are no bundles, return early.
   if (Object.keys(bundles).length === 0) {
@@ -80,48 +47,19 @@ export function run(input: RunInput): FunctionRunResult {
   }
 
   // Create merge operations for each bundle
-  const updates = Object.values(bundles).map((bundleLines) => {
-    const parentVariantId = bundleLines.find(
-      (line) => line.parentVariantId?.value
-    )?.parentVariantId?.value;
+  const updates = Object.values(bundles).map(({ items, parentVariantId, attributes }) => {
     if (!parentVariantId) return null
-
-    // Create attributes for the bundle
-    const attributes = {} as Record<string, { key: string, value: string }>
-    bundleLines.forEach((line) => {
-      if (line?.checkoutInfo?.value) {
-        attributes['_checkoutInfo'] = {
-          key: '_checkoutInfo',
-          value: line.checkoutInfo.value
-        }
-      }
-
-      if (line?.despatchDate?.value) {
-        attributes['_despatchDate'] = {
-          key: '_despatchDate',
-          value: line.despatchDate.value
-        }
-      }
-
-      if (line?.bundleSku?.value) {
-        attributes['_bundleSku'] = {
-          key: '_bundleSku',
-          value: line.bundleSku.value
-        }
-      }
-    })
-
-    const image = getProductImage(bundleLines)
 
     return {
       merge: {
-        cartLines: bundleLines.map((line) => ({
+        cartLines: items.map((line) => ({
           cartLineId: line.id,
           quantity: 1
         })),
         parentVariantId,
-        image,
-        attributes: Object.values(attributes)
+        attributes: Object.entries(attributes).filter(
+          ([_, value]) => value
+        ).map(([key, value]) => ({ key, value }))
       }
     }
   }).filter(Boolean) as FunctionRunResult["operations"]
